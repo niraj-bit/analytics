e5afa78faa22772b27d091bea4625d7b36459855
Uday Mishra <udaymishra@Udays-MacBook-Pro.local>
2021-05-15 19:10:59 +0530

    pricing cards adjusted

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,156 +8,156 @@ import TwitterIcon from '@material-ui/icons/Twitter';
 export const Footer = () => {
   var sectionStyle = {
     width: "100%",
-     height: "915px",
-     backgroundImage: `url(../images/bottom.jpg)`,
-     backgroundSize: "cover",
-     overflow: "hidden",
-   };
-    return (
-      <div>
-      <div style={sectionStyle}> 
-      <div class="container1">
-      <div class="para">
-      
-      <h3 class="text-center"><strong>Pricing plans</strong></h3>
-<p class="text-center">Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-</div>
+    height: "915px",
+    backgroundImage: `url(../images/bottom.jpg)`,
+    backgroundSize: "cover",
+    overflow: "hidden",
+  };
+  return (
+    <div>
+      <div style={sectionStyle}>
+        <div class="container1">
+          <div class="para">
 
-  
-{/* <div class="row">
+            <h3 class="text-center"><strong>Pricing plans</strong></h3>
+            <p class="text-center">Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+          </div>
+
+
+          {/* <div class="row">
   <div class="col-md-3 g-4">.col-md-3</div>
   <div class="col-md-3 g-4">.col-md-3</div>
   <div class="col-md-3 g-4">.col-md-3</div>
 </div> */}
-<div class="container">
-<div class="row">
-  <div class="col-md-3 g-4">
-    <div class="card">
-    <div class="first">
-      <div class="card-body">
-      <h4 >Starter</h4>
-      <div class="user0">upto 3 user</div>
-      <hr class="hori0"/>
-      <h2>$55</h2>
-      <div class="pm">per month</div>
-      {/* </div> */}
-      </div>
-      </div>
-    </div>
-  </div>
-  <div class="col-md-3 g-4">
-    <div class="card">
-    <div class="middle">
-      <div class="card-body">
-      <h4 >Standard</h4>
-      <div class="user0">upto 20 user</div>
-      <hr class="hori1"/>
-      <h2>$55</h2>
-      <div class="pm">per month</div>
-     
-      </div>
-    </div>
-    </div>
-  </div>
-  <div class="col-md-3 g-4">
-    <div class="card">
-    <div class="last">
-      <div class="card-body">
-      <h4>Premium</h4>
-      <div class="user0">upto 200 user</div>
-      <hr class="hori0"/>
-      <h2>$55</h2>
-      <div class="pm">per month</div>
-      
-      </div>
-    </div></div>
-  </div>
-</div>
-</div>
+          <div class="container">
+            <div class="row">
+              <div class="col-md-4 g-4">
+                <div class="card">
+                  <div class="first">
+                    <div class="card-body">
+                      <h4 >Starter</h4>
+                      <div class="user0">upto 3 user</div>
+                      <hr class="hori0" />
+                      <h2>$55</h2>
+                      <div class="pm">per month</div>
+                      {/* </div> */}
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4 g-4">
+                <div class="card">
+                  <div class="middle">
+                    <div class="card-body">
+                      <h4 >Standard</h4>
+                      <div class="user0">upto 20 user</div>
+                      <hr class="hori1" />
+                      <h2>$55</h2>
+                      <div class="pm">per month</div>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4 g-4">
+                <div class="card">
+                  <div class="last">
+                    <div class="card-body">
+                      <h4>Premium</h4>
+                      <div class="user0">upto 200 user</div>
+                      <hr class="hori0" />
+                      <h2>$55</h2>
+                      <div class="pm">per month</div>
+
+                    </div>
+                  </div></div>
+              </div>
+            </div>
+          </div>
 
 
 
-{/* footer nav */}
+          {/* footer nav */}
 
-<div class="bot3">
+          <div class="bot3">
 
-<nav class="navbar navbar-expand-lg">
-            <div class="container-fluid" alt="">
-              <a class="navbar-brand" href="#">
-                <img
-                  class="logo"
-                  src="/images/log.svg"
-                  alt=""
-                  width="30"
-                  height="24"
-                  class="d-inline-block align-text-top"
-                />
+            <nav class="navbar navbar-expand-lg">
+              <div class="container-fluid" alt="">
+                <a class="navbar-brand" href="#">
+                  <img
+                    class="logo"
+                    src="/images/log.svg"
+                    alt=""
+                    width="30"
+                    height="24"
+                    class="d-inline-block align-text-top"
+                  />
                 ANALYTICS
               </a>
-              <button
-                class="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">
-                      Products
+                <button
+                  class="navbar-toggler"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#navbarSupportedContent"
+                  aria-controls="navbarSupportedContent"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">
+                        Products
                     </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">
-                      Pricing
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">
+                        Pricing
                     </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">
-                      FAQ
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">
+                        FAQ
                     </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">
-                      Blog
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">
+                        Blog
                     </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#">
-                      Blog
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">
+                        Blog
                     </a>
-                  </li>
-                </ul>
-                <div class="navbar-nav ml-auto action-buttons">
-                  <div class="Login">
-                    <a href="#" class="nav-link mr-4">
-                    <FacebookIcon/>
-                    </a>
-                  </div>
-                  <div class="Login">
-                    <a href="#" class="nav-link mr-4">
-                    <InstagramIcon/>
-                    </a>
-                  </div>
-                  <div class="Login">
-                    <a href="#" class="nav-link mr-4">
-                    <TwitterIcon />
-                    </a>
-                  </div>
-                  
-                </div>
-                </div>
-                </div>
-                </nav>
-                </div>
+                    </li>
+                  </ul>
+                  <div class="navbar-nav ml-auto action-buttons">
+                    <div class="Login">
+                      <a href="#" class="nav-link mr-4">
+                        <FacebookIcon />
+                      </a>
+                    </div>
+                    <div class="Login">
+                      <a href="#" class="nav-link mr-4">
+                        <InstagramIcon />
+                      </a>
+                    </div>
+                    <div class="Login">
+                      <a href="#" class="nav-link mr-4">
+                        <TwitterIcon />
+                      </a>
+                    </div>
 
-</div>
-</div>
+                  </div>
+                </div>
+              </div>
+            </nav>
+          </div>
+
+        </div>
       </div>
-    )
+    </div>
+  )
 }
